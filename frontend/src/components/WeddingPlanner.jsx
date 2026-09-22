63fe2fc85e2463afbf611222b130c38eac44b349
wedoraai9-art <wedoraai9@gmail.com>
2026-09-23 05:20:19 +0530

    Update WeddingPlanner.jsx

--- frontend/src/components/WeddingPlanner.jsx
+++ frontend/src/components/WeddingPlanner.jsx
@@ -289,6 +289,9 @@ export default function WeddingPlanner() {
                     if (title === "Bride & Groom") {
                         window.location.href = "/wedding-planning/couple";
                       }
+                    if (title === "Transportation") {
+                          window.location.href = "/wedding-planning/transportation";
+                        }
                                       }}
                   className="group rounded-3xl border border-white bg-white p-6 text-left shadow-sm transition hover:-translate-y-1 hover:shadow-xl"
                 >
