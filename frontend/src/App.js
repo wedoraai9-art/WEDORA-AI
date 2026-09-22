c867a9dcea033c43f6d04b829f55ed1085272120
wedoraai9-art <wedoraai9@gmail.com>
2026-09-23 03:59:59 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -62,6 +62,7 @@ function App() {
             <Route path="/wedding-planning" element={<WeddingPlanner />} />
             <Route path="/wedding-planning/checklist" element={<WeddingChecklist />} />
             <Route path="/wedding-planning/timeline" element={<WeddingTimeline />} />
+            <Route path="/wedding-planning/guests" element={<WeddingGuests />} />
             <Route path="/for-vendors" element={<VendorLanding />} />
             <Route path="/vendor/auth" element={<VendorAuth />} />
             <Route path="/vendor/dashboard" element={<VendorDashboard />} />
